707da6cf6ed5957a0025f183a21c25328144e8ea
yakirevich <yakirevich@cs.huji.ac.il>
2020-03-09 19:56:39 +0200

    added auto rotation functionality

--- image_splitter.py
+++ image_splitter.py
@@ -2,7 +2,49 @@ import numpy as np
 import cv2
 import os
 from pathlib import Path
-import logging
+import math
+
+
+def rotate_image(image, angle):
+    """
+    rotate the image by an angle
+    :param image: original image
+    :param angle: rotation angle
+    :return: rotated image, with white background color
+    """
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR, borderValue=(255, 255, 255))
+    return result
+
+
+def find_rotation_angle(im):
+    """
+    Apply Canny edge detection and Probabilistic Hough Lines, then find the median rotation angle
+    :param im: image
+    :return: rotation angle in degrees
+    """
+    dst = cv2.Canny(im, 50, 200, None, 3)
+    cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    cdstP = np.copy(cdst)
+    linesP = cv2.HoughLinesP(dst, 1, np.pi / 180, 80, None, 50, 10)
+
+    angles = []
+    if linesP is not None:
+        for line in linesP:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(cdstP, (x1, y1), (x2, y2), (0, 0, 255), 3, cv2.LINE_AA)
+            angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
+            angles.append(angle)
+    angles = np.array(angles)
+    angles1 = angles[np.abs(angles) < 45]
+    median1 = np.median(angles1)
+    angles2 = angles[np.abs(angles) >= 45]
+    median2 = np.median(angles2)
+
+    if np.abs(median1) > np.abs(90 - np.abs(median2)):
+        return median1
+    return np.abs(90 - np.abs(median2)) * np.sign(-median2)
 
 
 def filter_contours(contours, sensitivity=.1):
@@ -22,21 +64,21 @@ def filter_contours(contours, sensitivity=.1):
     return filtered_contours
 
 
-def split(source_director):
+def split(source_directory):
     """
     Iterate over all photos in the directory and split them
-    :param source_director: traget directory
+    :param source_directory: traget directory
     :return: none
     """
-    logging.basicConfig(filename='filtered_photos.log', level=logging.INFO, format='%(message)s')
+    log = ''
     total_photos = 0
-    logging.info(f'Processing {source_director} directory')
-    for file in os.listdir(source_director):
+    log += f'Processing {source_directory} directory\n'
+    for file in os.listdir(source_directory):
         if not file.startswith('.'):  # ignore hidden files
             full_file_name = os.fsdecode(file)
-            file_path = source_director + '/' + full_file_name
+            file_path = source_directory + '/' + full_file_name
             file_name = os.path.splitext(file_path)[0].split('/')[-1]
-            directory_path = f"cropped/{file_name}_output/"
+            directory_path = f"{source_directory}_split/{file_name}_output/"
             Path(directory_path).mkdir(parents=True, exist_ok=True)
 
             im = cv2.imread(file_path)
@@ -46,14 +88,21 @@ def split(source_director):
             opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
             contours, hierarchy = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             filtered_contours = filter_contours(contours)
+            log += f'file {file_name} : {len(filtered_contours)} photos\n'
             for i, contour in enumerate(filtered_contours):
                 x, y, w, h = cv2.boundingRect(contour)
                 cropped = im[y:y + h, x:x + w]
-                cv2.imshow('crop', cropped)
-                cv2.imwrite(f'{directory_path}{file_name}_{i + 1}.jpg', cropped)
-            logging.info(f'file {file_name} : {len(filtered_contours)} photos')
+                cropped_opening = opening[y:y + h, x:x + w]
+                angle = find_rotation_angle(cropped_opening)
+                rotated = rotate_image(cropped, angle)
+                cv2.imwrite(f'{directory_path}{file_name}_{i + 1}.jpg', rotated)
+                log += f'\tsub-photo {i + 1}: rotation = {angle:.3f}\n'
             total_photos += len(filtered_contours)
-    logging.info(f'Total number of photos : {total_photos}')
+    log += f'Total number of photos : {total_photos}'
+
+    f = open('log.txt', 'w+')
+    f.write(log)
+    f.close()
 
 
 if __name__ == '__main__':
